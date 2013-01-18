9ba559bf2c13801f16a6652fc5f5440b44ae6f53
slimhero <dkrivets@head3178.(none)>
2013-01-18 18:28:39 +0600

    Change view dashboard

--- static/hbt/user/user.js
+++ static/hbt/user/user.js
@@ -1,11 +1,15 @@
 <div class="container">
+    <div class="row pull-right span1 txtalign-right" >
+      <a href="#"class="navbar-link" >Log out</a>
+    </div>
   <div class="row">
-    <div class="navbar span12">
+    <header class="navbar span12">
       <div class="navbar-inner">
         <a class="brand" href="#">taskViewer:</a>
         <ul class="nav">
           <li class="active"><a><b>{{ session.name }}</b></a></li>
-          <li><a href="#">Create New Project</a></li>
+          <li class="divider-vertical"></li>
+          <li><button class="btn btn-warning">Create New Project</a></li>
         </ul>
         <ul class="nav pull-right">
           <li>
@@ -13,27 +17,30 @@
               <input type="text" class="search-query" placeholder="Context search">
             </form>
           </li>
-          <li class="divider-vertical"></li>
-          <li>
-            <p class="navbar-text pull-right">
-              <a href="#"class="navbar-link" >Log out</a>
-            </p>
-          </li>
         </ul>
       </div>
-    </div>
+    </header>
 
+  </div>  
+  <div class="row">
     <!-- Docs nav
     ================================================== -->
-    <div class="span3 bs-docs-sidebar">
-      <ul class="nav nav-list bs-docs-sidenav a2ffix">
-        <li><a href="#messages"><i class="icon-chevron-right"></i> Messages</a></li>
-        <li><a href="#projects"><i class="icon-chevron-right"></i> Projects</a></li>
-        <li><a href="#tickets"><i class="icon-chevron-right"></i> Tickets</a></li>
-        <li><a href="#options"><i class="icon-chevron-right"></i> Options</a></li>
+    <div id="asd2" class="span3 bs-docs-sidebar">
+      <ul class="nav nav-list bs-docs-sidenav affix-top">
+        <li><a href="#messages"> Messages
+          <i class="icon-chevron-right"></i></a></li>
+        <li><a href="#projects"> Projects
+          <i class="icon-chevron-right"></i></a></li>
+        <li><a href="#tickets"> Tickets
+          <i class="icon-chevron-right"></i></a></li>
+        <li><a href="#options"> Options
+          <i class="icon-chevron-right"></i></a></li>
       </ul>
     </div>
     <!-- Contents
     ================================================== -->
+    <div id="asd" class="span9 bs-docs-sidebar nav  bs-docs-sidenav2 container-general">
+      <p>asdasdasdasdasdasd</p>
+    </div>
   </div>
 </div>
