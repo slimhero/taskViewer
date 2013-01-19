c81b9b7833f2a493cc8ebbf4845e7e14bf2d4dbb
SlimHero <nu_ne_figa_sebe@mail.ru>
2013-01-20 00:51:04 +0600

    Change dashboard template

--- static/hbt/user/user.js
+++ static/hbt/user/user.js
@@ -1,46 +1,62 @@
 <div class="container">
-    <div class="row pull-right span1 txtalign-right" >
-      <a href="#"class="navbar-link" >Log out</a>
-    </div>
   <div class="row">
-    <header class="navbar span12">
+	  <h3 class="pull-left span2">taskViewer</h3>
+	</div>
+  <div class="row">
+    <div class="navbar span12">
       <div class="navbar-inner">
-        <a class="brand" href="#">taskViewer:</a>
-        <ul class="nav">
+        <a class="brand" href="#">Dashboard</a>
+				<ul class="nav">
           <li class="active"><a><b>{{ session.name }}</b></a></li>
           <li class="divider-vertical"></li>
-          <li><button class="btn btn-warning">Create New Project</a></li>
+          <li><button class="btn">Create New Project</a></li>
+          <li class="divider-vertical"></li>
+          <li><button class="btn btn-warning">Options</a></li>
         </ul>
         <ul class="nav pull-right">
-          <li>
-            <form class="navbar-search pull-right">
-              <input type="text" class="search-query" placeholder="Context search">
-            </form>
-          </li>
+          <li><form class="navbar-search">
+            <input type="text" class="search-query" placeholder="Context search">
+          </form></li>
+					<li class="txtalign-right"><a href="#" class="navbar-link txtalign-right">Log out</a></li>
         </ul>
       </div>
-    </header>
+    </div>
 
   </div>  
   <div class="row">
     <!-- Docs nav
     ================================================== -->
-    <div id="asd2" class="span3 bs-docs-sidebar">
-      <ul class="nav nav-list bs-docs-sidenav affix-top">
+    <div id="menu" class="span2">
+      <ul class="nav nav-tabs nav-stacked affix-top">
+        <li><a href="#messages"> TODO-List
+          <i class="icon-chevron-right pull-right"></i></a></li>
         <li><a href="#messages"> Messages
-          <i class="icon-chevron-right"></i></a></li>
+          <i class="icon-chevron-right pull-right"></i></a></li>
         <li><a href="#projects"> Projects
-          <i class="icon-chevron-right"></i></a></li>
+          <i class="icon-chevron-right pull-right"></i></a></li>
         <li><a href="#tickets"> Tickets
-          <i class="icon-chevron-right"></i></a></li>
-        <li><a href="#options"> Options
-          <i class="icon-chevron-right"></i></a></li>
+          <i class="icon-chevron-right pull-right"></i></a></li>
       </ul>
     </div>
     <!-- Contents
     ================================================== -->
-    <div id="asd" class="span9 bs-docs-sidebar nav  bs-docs-sidenav2 container-general">
-      <p>asdasdasdasdasdasd</p>
-    </div>
+		<div class="span10">
+      <div id="bottom-line" class="well data-bar">
+        <div class="span2">
+			    <h2>0</h2>
+				  <h6>PROJECTS</h6>
+			  </div>  
+        <div class="span2">
+			    <h2>0</h2>
+				  <h6>TASKS</h6>
+			  </div>  
+        <div class="span2 last">
+			    <h2>0</h2>
+				  <h6>MESSAGES</h6>
+			  </div>  
+			    <h2>0</h2>
+				  <h6>TICKETS</h6>
+      </div>
+		</div>
   </div>
 </div>
