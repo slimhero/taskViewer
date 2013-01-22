3192e530bd097c18113848d8aff9e814bea348f9
slimhero <dkrivets@head3178.(none)>
2013-01-22 08:30:52 +0600

    session is made to return all session keys and values

--- services/sessionsrv.rb
+++ services/sessionsrv.rb
@@ -16,12 +16,20 @@ class Sessions < Sinatra::Base
   # Get user data by id
   get '/api/session' do  
     if session[:isAuth] == true 
-      { 
-        :session =>  {
-          :name => session[:name],
-          :isAuth => session[:isAuth]
-        } 
-      }.to_json
+      
+      #{ 
+      #  :session =>  {
+      #    :name => session[:name],
+      #    :isAuth => session[:isAuth]
+      #  } 
+      #}.to_json
+      #{ :session => session[:slug] }.to_json
+      hash = {};
+      session.each_pair do |key,value| #k and v class as in hash
+       #put "#{key}=#{value}"
+       hash[key] = value
+      end
+      { :session => hash }.to_json
     else
       { :error=>"Only for authorised users" }.to_json
     end
